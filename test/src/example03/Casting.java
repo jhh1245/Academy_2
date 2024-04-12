37822044821961ec62aea8824a5e8a411a31d98c
jhh1245 <hahaha_1026@naver.com>
2024-04-12 18:09:43 +0900

    04.12 3번째 업로드

---
+++ test/src/example03/Casting.java
@@ -0,0 +1,15 @@
+package example03;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int intVal = 10;
+		double doubleVal = 5.5;
+		// double result = intVal + doubleVal; //int가 double이 되서 결과값은 double이 된다. 
+		int result = intVal + (int)doubleVal; //10 + 5 = 15가 된다.  
+		
+		System.out.println(result);
+	}
+
+}
--- test/src/test/chapter2/example02/PromotionExample.java
+++ test/src/test/chapter2/example02/PromotionExample.java
@@ -7,16 +7,25 @@ public class PromotionExample {
 		int intValue = byteValue; //문제되지 않음 
 		System.out.println(intValue);
 		
-		char charVal = 'A'; //65
-		System.out.println(charVal);
+		int charVal = 'A';
+		System.out.println(charVal);  //65
 		
 		int intVal2 = 500;
 		long longVal = intVal2;
-		System.out.println(longVal);
+		System.out.println(longVal); //50
 		
 		intVal2 = 200;
 		double doubleVal = intVal2;
-		System.out.println(doubleVal);
+		System.out.println(doubleVal); //200.0
+		
+		intVal2 = 123456784;
+		//byteValue = intVal2; // 에러 int값이 더 크기 때문에 byte에 못담음
+		byteValue = (byte)intVal2; //int 값 4자리중에 3자리를 버린다. 
+		System.out.println(byteValue);
+		
+		doubleVal = 3.14;
+		intValue = (int)doubleVal;
+		System.out.println(intValue); //.14는 잘리고 3만 남는다. 
 	}
 
 }
